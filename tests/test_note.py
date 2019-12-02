3f01b4a191c23db4a00cba6183ed08f50b941a88
Jeff Moorhead <jeff.moorhead1@gmail.com>
2019-12-02 17:17:10 -0500

    Removed implementation details from test_descend_interval unit test.

--- tests/test_note.py
+++ tests/test_note.py
@@ -15,34 +15,8 @@ class TestNote(unittest.TestCase):
             self.assertEqual(self.a.ascend_interval(interval), Note(top_note))
 
     def test_descend_interval(self):
-        for note, pairs in INTERVAL_NOTE_PAIRS.items():
-            for interval, steps in INTERVAL_STEPS.items():
-                if steps >= 100:
-                    if steps % 10 == 6:  # augmented 4 compliment is a diminished 5
-                        steps_compliment = 6
-                    else:
-                        steps_compliment = 12 - (steps % 100) + 100
-                else:
-                    if steps == 6:  # diminished 5 compliment is an augmented 4
-                        steps_compliment = 106
-                    else:
-                        steps_compliment = 12 - steps
-
-                try:
-                    bottom_note = INTERVAL_NOTE_PAIRS[note][steps_compliment]
-                except KeyError:
-                    failure_msg = f"Starting_note: {note}, Bottom note: {bottom_note}, Steps: {steps}, Interval: {interval}"
-                    print(failure_msg)
-
-                this_note = Note(note)
-                # print('Starting note: %s\nBottom note: %s\nInterval: %s' % (note, bottom_note, interval))
-
-                failure_msg = f"Starting_note: {note}, Bottom note: {bottom_note}, Steps: {steps}, Interval: {interval}"
-                if bottom_note is None:
-                    with self.assertRaises(InvalidIntervalError):
-                        this_note.descend_interval(interval)
-                else:
-                    self.assertEqual(this_note.descend_interval(interval), Note(bottom_note), failure_msg)
+        c = self.a.descend_interval("MAJOR 6")
+        self.assertEqual(c, Note('C'))
 
     def test_invalid_qualified_name(self):
         with self.assertRaises(NoteNameError):
