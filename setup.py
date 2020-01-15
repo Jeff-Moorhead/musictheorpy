db550cc9bf16e50e8feb92b7270af690fdd29d93
Jeff Moorhead <jeff.moorhead1@gmail.com>
2020-01-14 21:19:59 -0500

    Increment version

--- setup.py
+++ setup.py
@@ -7,12 +7,11 @@ with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as readme:
 
 setuptools.setup(
     name="musictheorpy",
-    version="1.2.4",
+    version="1.2.6",
     author="Jeffrey Moorhead",
     author_email="jeff.moorhead1@gmail.com",
     description="A music theory library for Python",
     long_description=long_description,
-    long_description_content_type="text/x-rst",
     url="https://github.com/Jeff-Moorhead/musictheorpy",
     packages=setuptools.find_packages(),
     classifiers=[
