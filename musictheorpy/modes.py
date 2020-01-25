0d32617546f62b57029b625baa949e5a068e741a
Jeff Moorhead <jeff.moorhead1@gmail.com>
2020-01-25 10:07:58 -0500

    Initial modes commit

---
+++ musictheorpy/modes.py
@@ -0,0 +1,4 @@
+# Modes - a mode is a note group, similar to a scale, but is based off of a certain degree of the corresponding scale.
+# For example, D dorian is a C scale starting on D. So, one possible approach is to build the scale, find the degree
+# that corresponds to the mode's 'tonic', then rearrange the notes in the scale to start on the mode's tonic. This
+# will require adding a new method to scales to get the degree based on a given note. ---- Start there.
